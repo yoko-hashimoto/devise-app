baacb59204dc8f44d8a548c671f127bd8a2f99e4
yoko <yoko.hashimoto.29@gmail.com>
2019-03-05 10:49:07 +0900

    deviseを導入

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  root 'blogs#index' #追記
+  
+  resources :blogs
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+end
