73a4da7f7e2b47f4897d89befaea9239f601dc98
yoko <yoko.hashimoto.29@gmail.com>
2019-03-07 08:22:40 +0900

    development.rbを修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'devise--app.herokuapp.com'}
   config.action_mailer.delivery_method = :letter_opener
 
 end
